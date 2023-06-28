9aac4976da86484123392655ea13620085cb8aa6
d-sayali <sayali08d@gmail.com>
2023-06-29 02:12:32 +0530

    db change for hs code search

--- app/controllers/hs_code.controller.js
+++ app/controllers/hs_code.controller.js
@@ -21,7 +21,7 @@ exports.getHsCodeDetails = async (req, res) => {
 	let _q = "";
 	if (_hs) {
 		if (hs.length % 2 !== 0) { return returnError(res, "Please enter 2, 4 or 6 digit hs code", 400); }
-		_q = `SELECT concat(hs2,' - ',hs2_des) AS hs2, concat(hs4,' - ',hs4_des) AS hs4, concat(hs6,' - ',hs6_des) AS hs6 FROM hs_codes_details WHERE hs2 = '${hs}' OR hs4 = '${hs}' OR hs6 = '${hs}' limit 100;`;
+		_q = `SELECT concat(product,' - ',hs_code) AS hs6 FROM hs_codes WHERE product LIKE '${hs}%' limit 100;`;
 	}
 	else {
 		_q = `SELECT concat(product,' - ',hs_code) AS hs6 FROM hs_codes WHERE hs_code LIKE '%${hs}%' limit 100;`;
